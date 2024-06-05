2a44d322de6b51d09d8fe31225afe58e5988270e
legaspi0621 <christianryle.legaspi@gmail.com>
2024-06-05 15:27:12 +0800

    dev

--- src/pages/StatisticsPage.js
+++ src/pages/StatisticsPage.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import SmokeStatistics from '../components/SmokeStatistics'
 import { Pie } from 'react-chartjs-2'
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
 import './StatisticsPage.css'
