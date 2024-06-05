111f46a5d4d91060daee92435c7c845604c554ad
legaspi0621 <christianryle.legaspi@gmail.com>
2024-06-05 19:44:10 +0800

    test

--- src/App.js
+++ src/App.js
@@ -1,49 +1,44 @@
-import { BrowserRouter, Routes, Route, Navigate, useLocation } from 'react-router-dom';
-import { useAuthContext } from './hooks/useAuthContext';
+import { BrowserRouter, Routes, Route, Navigate, useLocation } from 'react-router-dom'
+import { useAuthContext } from './hooks/useAuthContext'
 
 // pages & components
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Dashboard from './pages/DashboardPage'; // Ensure this import path is correct
-import Sidebar from './components/Sidebar';
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Sidebar from './components/Sidebar'
 
 function App() {
-  const { user } = useAuthContext();
-  const location = useLocation();
-  const showSidebar = location.pathname !== '/login' && location.pathname !== '/signup';
+  const { user } = useAuthContext()
+  const location = useLocation()
+  const showSidebar = location.pathname !== '/login' && location.pathname !== '/signup'
 
   return (
     <div className="App">
       {showSidebar && <Sidebar />}
       <div className={showSidebar ? "content-with-sidebar" : "content"}>
         <Routes>
-          <Route
-            path="/"
-            element={user ? <Home /> : <Navigate to="/login" />}
+          <Route 
+            path="/" 
+            element={user ? <Home /> : <Navigate to="/login" />} 
           />
-          <Route
-            path="/dashboard"
-            element={user ? <Dashboard /> : <Navigate to="/login" />}
+          <Route 
+            path="/login" 
+            element={!user ? <Login /> : <Navigate to="/" />} 
           />
-          <Route
-            path="/login"
-            element={!user ? <Login /> : <Navigate to="/" />}
-          />
-          <Route
-            path="/signup"
-            element={!user ? <Signup /> : <Navigate to="/" />}
+          <Route 
+            path="/signup" 
+            element={!user ? <Signup /> : <Navigate to="/" />} 
           />
         </Routes>
       </div>
     </div>
-  );
+  )
 }
 
 const AppWithRouter = () => (
   <BrowserRouter>
     <App />
   </BrowserRouter>
-);
+)
 
-export default AppWithRouter;
+export default AppWithRouter
