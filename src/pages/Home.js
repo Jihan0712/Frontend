a322d3360ddf1aab337ed2a52ca12ff918426ed1
legaspi0621 <christianryle.legaspi@gmail.com>
2024-06-05 19:18:26 +0800

    revert

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,66 +1,42 @@
-import { useEffect, useState } from "react";
-import { useSmokesContext } from "../hooks/useSmokesContext";
-import { useAuthContext } from "../hooks/useAuthContext";
+import { useEffect } from "react"
+import { useSmokesContext } from "../hooks/useSmokesContext"
+import { useAuthContext } from "../hooks/useAuthContext"
 
 // components
-import SmokeDetails from "../components/SmokeDetails";
-import SmokeForm from "../components/SmokeForm";
+import SmokeDetails from "../components/SmokeDetails"
+import SmokeForm from "../components/SmokeForm"
 
 const Home = () => {
-  const { smokes, dispatch } = useSmokesContext();
-  const { user } = useAuthContext();
-  const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 5;
+  const { smokes, dispatch } = useSmokesContext()
+  const {user} = useAuthContext()
 
   useEffect(() => {
     const fetchSmokes = async () => {
       const response = await fetch('https://backend-ieyu.onrender.com/api/Smokes', {
-        headers: { 'Authorization': `Bearer ${user.token}` },
-      });
-      const json = await response.json();
+        headers: {'Authorization': `Bearer ${user.token}`},
+      })
+      const json = await response.json()
 
       if (response.ok) {
-        dispatch({ type: 'SET_SMOKES', payload: json });
+        dispatch({type: 'SET_SMOKES', payload: json})
       }
-    };
+    }
 
     if (user) {
-      fetchSmokes();
+      fetchSmokes()
     }
-  }, [dispatch, user]);
-
-  const handlePageChange = (pageNumber) => {
-    setCurrentPage(pageNumber);
-  };
-
-  // Calculate the data to be displayed on the current page
-  const indexOfLastItem = currentPage * itemsPerPage;
-  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const currentItems = smokes ? smokes.slice(indexOfFirstItem, indexOfLastItem) : [];
-
-  const totalPages = Math.ceil(smokes ? smokes.length / itemsPerPage : 0);
+  }, [dispatch, user])
 
   return (
     <div className="home">
       <div className="smokes">
-        {currentItems && currentItems.map((smoke) => (
+        {smokes && smokes.map((smoke) => (
           <SmokeDetails key={smoke._id} smoke={smoke} />
         ))}
       </div>
       <SmokeForm />
-      <div className="pagination">
-        {Array.from({ length: totalPages }, (_, index) => (
-          <button
-            key={index + 1}
-            onClick={() => handlePageChange(index + 1)}
-            className={currentPage === index + 1 ? 'active' : ''}
-          >
-            {index + 1}
-          </button>
-        ))}
-      </div>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
