fc1b64dff846d112f54baeb90b74aaa2ce7ae289
legaspi0621 <christianryle.legaspi@gmail.com>
2024-06-05 17:16:16 +0800

    removed

--- src/pages/StatisticsPage.js
+++ src/pages/StatisticsPage.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Pie, Bar } from 'react-chartjs-2';
-import Chart from 'chart.js/auto';
+// Removed the unused import
 import './StatisticsPage.css';
 
 const StatisticsPage = () => {
